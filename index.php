1f5be538cc6dcae92213288ade92e1a8681cad7b
naong69 <naong69@gmail.com>
2014-05-09 13:34:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+ <head>
+  <title> New Document </title>
+   <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+  <script src="css/bootstrap-3.1.1-dist/css/bootstrap.min.js"></script>
+  <link type="text/css" rel="stylesheet" href="css/style.css" />
+ </head>
+
+ <body>
+  
+ </body>
+</html>
+<?php
+
+?>
---
+++ template.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+ <head>
+  <title> New Document </title>
+   <script src="js/jquery/jquery-1.10.2.min.js"></script>
+  <link type="text/css" rel="stylesheet" href="css/style.css" />
+  <link type="text/css" rel="stylesheet" href="bootstrap-3.1.1-dist/css/bootstrap.min.css" />
+ </head>
+
+ <body>
+  
+ </body>
+</html>
+<?php
+
+?>
